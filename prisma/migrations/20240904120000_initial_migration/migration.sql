fc71142a1ad468596008669859042cfaec17db6b
Marco <maguaman2@sudamericano.edu.ec>
2024-09-06 17:07:18 -0500

    regiter works

--- prisma/migrations/20240904120000_initial_migration/migration.sql
+++ prisma/migrations/20240904120000_initial_migration/migration.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
-  fullname TEXT NOT NULL,
-  isActive BOOLEAN DEFAULT TRUE,
+  full_name TEXT NOT NULL,
+  is_active BOOLEAN DEFAULT TRUE,
   roles TEXT[] DEFAULT ARRAY['user']
 );
